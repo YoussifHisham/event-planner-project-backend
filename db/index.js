68cf49be169b23227252edfabbeac4cd238dd116
YoussifHisham <youssefhishamxx@gmail.com>
2025-12-02 15:31:55 +0200

    docker file commited

--- db/index.js
+++ db/index.js
@@ -1,13 +1,16 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Create a new connection pool using the Database URL from .env
+// Create a new connection pool using individual environment variables
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Export a query function that logs the query text
-// This allows us to use this pool in other files
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
